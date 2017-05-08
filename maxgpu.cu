22e2131e8c66ca0990cabd7774e3747b82184e0b
CorbinLee <corbindlee@gmail.com>
2017-05-07 23:00:32 -0400

    aa

--- maxgpu.cu
+++ maxgpu.cu
@@ -4,9 +4,9 @@
 #include <cuda.h>
 #include <math.h>
 
-unsigned int getmaxcu(unsigned int *, unsigned int *, unsigned int);
+unsigned int getmaxcu(unsigned int *, unsigned int *, unsigned int, unsigned int);
 __global__
-void foo(unsigned int *, unsigned int *, unsigned int);
+void foo(unsigned int *, unsigned int *, unsigned int, int);
 
 int main(int argc, char *argv[])
 {
@@ -14,6 +14,8 @@ int main(int argc, char *argv[])
     unsigned int i;  // loop index
     unsigned int * numbers; //pointer to the array
     unsigned int * max;
+    unsigned int numThreads;
+    unsigned int numsPerThread;
     
     if(argc !=2)
     {
@@ -24,8 +26,14 @@ int main(int argc, char *argv[])
    
     size = atol(argv[1]);
 
+    /* Give each thread 1000 numbers */
+    numsPerThread = 1000;
+
+    /* I'm assuming that size is divisible by 1000 since that's the case for all the tests for the writeup */
+    numThreads = size / numsPerThread;
+
     numbers = (unsigned int *)malloc(size * sizeof(unsigned int));
-    max = (unsigned int *)malloc(10 * sizeof(unsigned int));
+    max = (unsigned int *)malloc(numThreads * sizeof(unsigned int));
     if( !numbers )
     {
        printf("Unable to allocate mem for an array of size %u\n", size);
@@ -38,7 +46,7 @@ int main(int argc, char *argv[])
        numbers[i] = rand()  % size;    
    
     printf(" The maximum number in the array is: %u\n", 
-        getmaxcu(numbers, max, size));
+        getmaxcu(numbers, max, size, numThreads));
 
     free(numbers);
     exit(0);
@@ -50,7 +58,7 @@ int main(int argc, char *argv[])
           number of elements in the array
    output: the maximum number of the array
 */
-unsigned int getmaxcu(unsigned int * num, unsigned int * max, unsigned int size)
+unsigned int getmaxcu(unsigned int * num, unsigned int * max, unsigned int size, unsigned int numThreads)
 {
     unsigned int i;
     unsigned int * max_d;
@@ -62,7 +70,7 @@ unsigned int getmaxcu(unsigned int * num, unsigned int * max, unsigned int size)
     cudaMemcpy(num_d, num, memSize, cudaMemcpyHostToDevice);
 
     /* Allocate space for the max number */
-    cudaMalloc((void **) &max_d, 10 * sizeof(unsigned int));
+    cudaMalloc((void **) &max_d, numThreads * sizeof(unsigned int));
 
     /** 
      * Kernel invocation code
@@ -73,21 +81,27 @@ unsigned int getmaxcu(unsigned int * num, unsigned int * max, unsigned int size)
      */
     //int threads = (30720 > size) ? size : 30720;
     
-    //int numsPerThread = 0;
-    // Find number of threads, rounding up
+    /* Find number of threads, rounding up */
     //int minNumThreads = (size + numsPerThread - 1) / numsPerThread;
-    int blocks = 1;
-    int threadsPerBlock = 10;
-    foo<<<blocks, threadsPerBlock>>>(num_d, max_d, size);
+
+    int threadsPerBlock = (numThreads > 1000) ? 1000 : numThreads;
+    int blocks = numThreads / threadsPerBlock;
+    int chunk = size / numThreads; // How many numbers each thread will process 
+
+    /* Invoke the kernel */
+    foo<<<blocks, threadsPerBlock>>>(num_d, max_d, size, chunk);
 
     /* Copy the max array from device to host and find the max of those */
-    cudaMemcpy(max, max_d, 10 * sizeof(unsigned int), cudaMemcpyDeviceToHost);
+    cudaMemcpy(max, max_d, numThreads * sizeof(unsigned int), cudaMemcpyDeviceToHost);
 
 
     unsigned int maxNum = 0;
-    for(i = 0; i < 10; i++)
-        if(max[i] > maxNum)
-            maxNum = max[i];
+    unsigned int cur;
+    for(i = 0; i < numThreads; i++) {
+        cur = max[i];
+        if(cur > maxNum)
+            maxNum = cur;
+    }
 
     cudaFree(num_d);
     cudaFree(max_d);
@@ -98,16 +112,24 @@ unsigned int getmaxcu(unsigned int * num, unsigned int * max, unsigned int size)
 
 /* Each thread will check it's portion of the array and put the max number in the first index */
 __global__
-void foo(unsigned int * num_d, unsigned int * max_d, unsigned int size) {
+void foo(unsigned int * num_d, unsigned int * max_d, unsigned int size, int chunk) {
     int i = threadIdx.x;
-    int chunk = size / 10;
     int j;
-    max_d[i] = 0;
+    int start = i * chunk;
+    int end = (i+1) * chunk;
 
-    for (j = i*chunk; j < (i+1) * chunk; j++) {
-        if (num_d[j] > max_d[i])
-            max_d[i] = num_d[j];
+    /* Local variables to reduce global memory reads */
+    unsigned int cur;
+    unsigned int maxNum = 0;
+
+
+    for (j = start; j < end; j++) {
+        cur = num_d[j];
+        if (cur > maxNum)
+            maxNum = cur;
     }
+
+    max_d[i] = maxNum;
 }
 
 
